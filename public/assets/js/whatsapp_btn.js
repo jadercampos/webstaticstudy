04ba4a36cbbbe394e7d4fd40e8accf74e4f62c6e
Jader Campos <jader.campos@gmail.com>
2022-05-19 11:21:56 -0300

    Including WhatsApp Widget, favicon, headers and layouts improvements

---
+++ public/assets/js/whatsapp_btn.js
@@ -0,0 +1,22 @@
+(function (w, d, s, u) {
+    w.gbwawc = {
+    url: u,
+    options: {
+            waId: "+55 (11) 97828-5612",
+            siteName: "Hospital Veterinário VIDAS",
+            siteTag: "Estamos Online!",
+            siteLogo: "https://webstaticstudy-cda1c.web.app/assets/img/logo/Logo.png",
+            widgetPosition: "RIGHT",
+            triggerMessage: "Chamar no WhatsApp",
+            welcomeMessage: "Olá, td bem?",
+            brandColor: "#25D366",
+            messageText: "Estou entrando em contato pelo site VIDAS, e gostaria de:\n",
+            replyOptions: ['Agendar uma consulta!','Outros assuntos'],
+        },
+    };
+    var h = d.getElementsByTagName(s)[0],
+    j = d.createElement(s);
+    j.async = true;
+    j.src = u + "/whatsapp-widget.min.js?_=" + Math.random();
+    h.parentNode.insertBefore(j, h);
+    })(window, document, "script", "https://waw.gallabox.com");
